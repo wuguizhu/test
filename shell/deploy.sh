f03cd4d51390a765ca4922f6a1cbc9696bbd88e7
Leirx <185734549@qq.com>
2019-12-24 17:31:14 +0800

    Update deploy.sh

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -6,7 +6,7 @@ wget https://github.com/wuguizhu/test/archive/master.zip
 unzip -o master.zip
 cd test-master
 chmod 777 testnode-pinger
-sudo ./testnode-pinger & 
+(sudo ./testnode-pinger &) 
 if [ $? -ne 0 ]; then
     echo "testnode-pinger deploy failed!"
 else
