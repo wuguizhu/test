57ce51cf9e09ec80ca4e4cd1347ae9946e480d74
Leirx <185734549@qq.com>
2019-12-27 16:46:45 +0800

    Update clean.sh

--- shell/clean.sh
+++ shell/clean.sh
@@ -3,7 +3,7 @@ echo ".........start to clean up............"
 rm -rf ../test-master
 echo "cleaning up /test/ folders finished..."
 rm -rf ../master.zip*
-rm -f ../deploy.sh
+rm -f ../deploy.sh*
 cd ..
 echo "cleaning up zip files finished........"
 echo ".........finish clean up.............."
