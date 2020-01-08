c7a2dd4b213d1d9c87885ed94d435bd5b1649ec3
何柳 <wb-hl653442@alibaba-inc.com>
2020-01-08 15:14:37 +0800

    暂时撤销修修复，思考新的修复方案

--- ping/tcp_ping.go
+++ ping/tcp_ping.go
@@ -19,7 +19,7 @@ func TcpOptionFromConf(conf *util.Conf) *util.TcpPingOption {
 		IntervalMs:     conf.TCPPingInterval,
 	}
 }
-func TCPPing(ipsGetter util.IPsGetter, conf *util.Conf) map[util.PingIP]*util.Statistics {
+func TCPPing(ipsGetter util.IPsGetter, conf *util.Conf) map[string]*util.Statistics {
 	localCN, err := time.LoadLocation("Asia/Shanghai")
 	if err != nil {
 		logs.Error("time.LoadLocation failed with err:", err)
@@ -27,16 +27,16 @@ func TCPPing(ipsGetter util.IPsGetter, conf *util.Conf) map[util.PingIP]*util.St
 	}
 	pingTime := time.Now().In(localCN).Format("2006-01-02 15:04:05")
 	pips := ipsGetter.GetIPs()
-	// ips := make([]string, 0, len(pips))
-	// for _, pip := range pips {
-	// 	ips = append(ips, pip.IP)
-	// }
+	ips := make([]string, 0, len(pips))
+	for _, pip := range pips {
+		ips = append(ips, pip.IP)
+	}
 	tcppingOption := TcpOptionFromConf(conf)
 	times := tcppingOption.Count
-	allRes := make(map[util.PingIP]*util.Statistics)
-	for _, ip := range pips {
+	allRes := make(map[string]*util.Statistics)
+	for _, ip := range ips {
 		s := util.NewStatistics(times)
-		allRes[*ip] = s
+		allRes[ip] = s
 		s.TcpPingResult.PingAtTime = pingTime
 	}
 	sipMap := make(map[string]string)
@@ -49,8 +49,8 @@ func TCPPing(ipsGetter util.IPsGetter, conf *util.Conf) map[util.PingIP]*util.St
 			time.Sleep(10 * time.Second)
 			continue
 		}
-		for _, pip := range pips {
-			err := tcpp.AddIP(pip.IP)
+		for _, ip := range ips {
+			err := tcpp.AddIP(ip)
 			if err != nil {
 				logs.Error("TCPPing add ip fails with error:", err)
 				time.Sleep(10 * time.Second)
@@ -65,9 +65,9 @@ func TCPPing(ipsGetter util.IPsGetter, conf *util.Conf) map[util.PingIP]*util.St
 		}
 		sipMap = srcMap
 		//此遍历没有统计丢包的rtt
-		for pip, s := range allRes {
+		for ip, s := range allRes {
 			s.SentPackets++
-			rtt, ok := res[pip.IP]
+			rtt, ok := res[ip]
 			if !ok {
 				s.LossPackets++
 				continue
@@ -76,12 +76,12 @@ func TCPPing(ipsGetter util.IPsGetter, conf *util.Conf) map[util.PingIP]*util.St
 			s.TotalRtt += rtt
 			s.Rtts = append(s.Rtts, rtt)
 		}
-		for pip, s := range allRes {
-			sip := sipMap[pip.IP]
+		for ip, s := range allRes {
+			sip := sipMap[ip]
 			s.Calculate()
-			logs.Debug("ip %s,srcIP %s,result %s", pip.IP, sip, s.TcpPingResult.String())
+			logs.Debug("ip %s,srcIP %s,result %s", ip, sip, s.TcpPingResult.String())
 		}
-		logs.Debug("tcp ping round:%d finished,ip count %d,use time %s", i, len(pips), time.Now().Sub(start))
+		logs.Debug("tcp ping round:%d finished,ip count %d,use time %s", i, len(ips), time.Now().Sub(start))
 	}
 	return allRes
 }
